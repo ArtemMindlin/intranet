abb87682818552978837989ed19b9b7bb8179582
ArtemMindlin <mindlinartem09@gmail.com>
2026-02-11 16:13:20 +0100

    Solucionado problema settings.py

--- intranet/settings/production.py
+++
@@ -1,8 +0,0 @@
-from .base import *
-
-DEBUG = False
-# ALLOWED_HOSTS = ["localhost", ""]
-
-# Si vais con HTTPS (recomendado) activa esto cuando esté bien montado
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True
